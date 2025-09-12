1855272e78b4db09b4ec0081a8e0709e80ee3b3a
unknown <carrieblackwood30@gmail.com>
2025-09-12 09:26:25 +0500

    added kz lang

--- index.js
+++ index.js
@@ -2,6 +2,44 @@ const modalOverlay = document.getElementById('modalOverlay');
 const modalTitle = document.getElementById('modalTitle');
 const modalList = document.getElementById('modalList');
 const closeModal = document.getElementById('closeModal');
+const kzBlock = document.querySelector('.kz');
+const ruBlock = document.querySelector('.ru');
+const ruBtns = document.querySelectorAll('.langRu');
+const kzBtns = document.querySelectorAll('.langKz');
+
+ruBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    localStorage.setItem('lang', 'ru');
+    langChoose();
+  });
+});
+
+kzBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    localStorage.setItem('lang', 'kz');
+    langChoose();
+  });
+});
+
+const langChoose = () => {
+  let lang = localStorage.getItem('lang');
+  if (!lang) {
+    lang = 'ru'; // язык по умолчанию
+    localStorage.setItem('lang', lang);
+  }
+
+  if (lang === 'kz') {
+    kzBlock.classList.remove('hide');
+    kzBlock.classList.add('show');
+    ruBlock.classList.remove('show');
+    ruBlock.classList.add('hide');
+  } else {
+    ruBlock.classList.remove('hide');
+    kzBlock.classList.remove('show');
+    ruBlock.classList.add('show');
+    ruBlock.classList.remove('hide');
+  }
+};
 
 document.querySelectorAll('.action__checkLists__detailsBtn').forEach(btn => {
   btn.addEventListener('click', () => {
@@ -24,3 +62,5 @@ modalOverlay.addEventListener('click', (e) => {
     modalOverlay.classList.remove('show');
   }
 });
+
+langChoose();
